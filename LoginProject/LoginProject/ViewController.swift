da609853896a7a98f696b3850230504898a3d514
zzee22su <patrickstar0910@gmail.com>
2023-06-11 13:59:39 +0900

    Add emailTextField autolayout

---
+++ LoginProject/LoginProject/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  LoginProject
+//
+//  Created by zzee22su on 2023/06/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let emailTextFieldlView = UIView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        makeUI()
+    }
+
+    func makeUI() {
+        emailTextFieldlView.backgroundColor = UIColor.darkGray
+        emailTextFieldlView.layer.cornerRadius = 8
+        emailTextFieldlView.layer.masksToBounds = true
+        
+        //기존 뷰가 아닌 다른 뷰를 올리기 위해 필요한 명령어
+        view.addSubview(emailTextFieldlView)
+        
+        //자동으로 프레임기준으로 오토레이아웃을 올려주는 기능을 해제 -> 지금은 수동으로 설정해야 되기 때문
+        emailTextFieldlView.translatesAutoresizingMaskIntoConstraints = false
+        
+        //오토레이아웃 잡기
+        emailTextFieldlView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30).isActive = true
+        emailTextFieldlView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30).isActive = true
+        emailTextFieldlView.topAnchor.constraint(equalTo: view.topAnchor, constant: 200).isActive = true
+        emailTextFieldlView.heightAnchor.constraint(equalToConstant: 40).isActive = true
+    }
+
+}
+
